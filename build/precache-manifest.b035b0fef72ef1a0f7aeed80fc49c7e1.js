eedff1a4517a0e9ac658d5f95f82069d6e3eb556
Kapilbindal1 <Kapilbindal1@gmail.com>
2019-10-10 16:20:12 +0530

    initial commit

---
+++ build/precache-manifest.b035b0fef72ef1a0f7aeed80fc49c7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c206e13f2c0dc15039aa68d926387658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c10218ef2f56d1015d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "17f94e098b40d5dc3f1e",
+    "url": "/static/js/2.3a94c9f6.chunk.js"
+  },
+  {
+    "revision": "05c10218ef2f56d1015d",
+    "url": "/static/js/main.c47d7194.chunk.js"
+  },
+  {
+    "revision": "2e02891c6f132d81631f",
+    "url": "/static/js/runtime-main.e0f62468.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
